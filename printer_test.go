a82a266ab4c35e79a7437adc4e4c8af1d10832c3
szktkfm <tk4fm1@gmail.com>
2024-03-24 00:53:27 +0900

    add tests

--- printer_test.go
+++ printer_test.go
@@ -2,6 +2,8 @@ package mdtt
 
 import (
 	"bytes"
+	"fmt"
+	"os"
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
@@ -26,4 +28,32 @@ func TestFindSegment(t *testing.T) {
 	if diff := cmp.Diff(want, got); diff != "" {
 		t.Errorf("differs: (-want +got)\n%s", diff)
 	}
+}
+
+func TestReplaceTable(t *testing.T) {
+	tw := TableWriter{}
+	fp, _ := os.Open("testdata/replace01.md")
+	defer fp.Close()
+
+	m := NewRoot(
+		WithMDFile("testdata/replace_want01.md"),
+	)
+
+	tw.render(m.table)
+	b := tw.replaceTable(fp)
+
+	fmt.Println(string(b))
+	// fp2, _ := os.Open("testdata/replace_want01.md")
+	// defer fp2.Close()
+	// want, _ := io.ReadAll(fp2)
+	want := `# Title
+| foo2                               | bar                                 |
+| -----------------------------------| ----------------------------------- |
+| bazaaaa                            | bim                                 |
+`
+
+	if diff := cmp.Diff(string(want), string(b)); diff != "" {
+		t.Errorf("differs: (-want +got)\n%s", diff)
+	}
+
 }
